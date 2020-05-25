b3bdabc177b81e7649f9b0e10f0d4b0447919ce7
dipeshtech <dipesh.nepali@gmail.com>
2020-05-25 11:18:33 +0545

    String manipulation

--- src/StringManipulation.java
+++ src/StringManipulation.java
@@ -1,10 +1,11 @@
 public class StringManipulation {
     public static void main(String[] args) {
         //Index pos= 0123456789012345
-        String str= "This is a string";
+        String str= "Dis strin";
         System.out.println(str.charAt(5)); //i
-        System.out.println(str.substring(0,4)); //here, you need to be careful to have 'This printed'
+        System.out.println(str.substring(0,5)); //here, you need to be careful to have 'This printed'
                                                 //you will need to go from 0,4 and not 0,3
-        System.out.println(str.substring(10,16));
+        System.out.println(str.substring(4,9));
+        System.out.println(str.length());
     }
 }
