b74081c45e53a163aa8c4fa758917973f6601c8a
dipeshtech <dipesh.nepali@gmail.com>
2020-05-28 15:39:58 +0545

    String Manipulation Switch cases

--- src/MyTest.java
+++ src/MyTest.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+
 public class MyTest {
+
     //Do not edit the main function
     public static void main(String[] args) {
+
         String input;
+
         //Get the input
         Scanner scanner = new Scanner(System.in);
         input = scanner.nextLine();
+
         //Print output returned from the logic function
         System.out.println(logic(input));
     }
@@ -14,14 +19,24 @@ public class MyTest {
     //The function is expected to return a String.
     //The function accepts String as parameter.
     public static String logic(String input){
-        input= input.toLowerCase();
-        String ans= "";
-        for (int i=0;i<input.length();i++){
-            String l= String.valueOf(input);
-        }
         //Write your code here
         //You can create other functions and call from here
         //Don't print anything. Just return the intended output
-        return input;
+        input= input.toLowerCase();
+        String ans= "";
+        for (int i=0;i<input.length();i++){
+            String l= String.valueOf(input.charAt(i));
+            if (l.equals("i") || l.equals("e")||l.equals("i")||l.equals("o")||l.equals("u")){
+                ans+=l;
+            }
+            else if (l.equals(" ")){
+                ans+=l;
+            }
+            else{
+                ans+=l.toUpperCase();
+            }
+        }
+        return ans;
+
     }
 }
