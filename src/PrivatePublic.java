fa8744cb2130683a6ed90c5794cd7364e9701fc7
dipeshtech <dipesh.nepali@gmail.com>
2020-05-31 09:00:14 +0545

    Access Modifier

---
+++ src/PrivatePublic.java
@@ -0,0 +1,10 @@
+
+public class PrivatePublic {
+    public static void main(String[] args) {
+        System.out.println("Hello there");
+    }
+}
+
+//private class Yo { //You can't make a private class on the outer region. It's only inside a class when
+//you create an inner class
+
