994fc4e82f53ab2f86ade3f2d1a2fd7aae6138c9
dipeshtech <dipesh.nepali@gmail.com>
2020-06-02 08:21:14 +0545

    Case Sensativity

---
+++ src/CaseSensativity.java
@@ -0,0 +1,10 @@
+public class CaseSensativity {
+    public static void main(String[] args) {
+        int a= 10;
+        int A= 5;
+        System.out.println(a);
+        System.out.println(A);
+        System.out.println(a-A);
+        System.out.println(A-a);
+    }
+}
